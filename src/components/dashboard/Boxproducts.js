4d83d97d50b3c2b77e456c7fc1a140b2c4a240f4
Abhishek Verma <125495260+Abhishekve-003@users.noreply.github.com>
2024-03-13 14:14:08 +0530

    Update Boxproducts.js

--- src/components/dashboard/Boxproducts.js
+++ src/components/dashboard/Boxproducts.js
@@ -9,7 +9,7 @@ const Boxproducts = () => {
     <>
        <Paper sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}} >
                 <Typography fontWeight="bold">Total Products</Typography>
-                <Typography variant='subtitle2'>16k</Typography>
+                <Typography variant='subtitle2'>160k</Typography>
                 <Growchart2/>
                 <Typography variant='subtitle2' style={{ color: accent }}>+40k</Typography>
                 <Typography variant='body2'>in last year</Typography>
